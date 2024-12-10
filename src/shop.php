b4c4f96339e9d322c15961dee21f9f8eaa4e3e4a
REACH-DEL <tirachlo34@gmail.com>
2024-12-10 15:07:12 +0700

    get

--- src/shop.php
+++ src/shop.php
@@ -9,10 +9,10 @@
     <title>CAFE</title>
 </head>
 <body>
-    <section class="sm:flex ">
+    <section class="">
         <?php include('../utilities/nav.php')?>
     
     </section>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    
 </body>
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{jsp,js}"],
+  content: ["./src/**/*.{php,js}"],
   theme: {
     extend: {
       colors: {
