bc4af6c68a7574a2f7dc0636678064f56640d3f6
REACH-DEL <tirachlo34@gmail.com>
2024-12-11 16:01:11 +0700

    Your commit message

--- utilities/nav.php
+++ utilities/nav.php
@@ -21,7 +21,7 @@
     </li>
     <li class="nav-item">
       <a class="nav-link" href="./cart.php"
-        ><img src="./asset/icon/cart.png" alt="" />Cart</a
+        ><img src="../asset/icon/cart.png" alt="" />Cart</a
       >
     </li>
   </ul>
@@ -30,35 +30,3 @@
     <div class="user-name">user1</div>
   </div>
 </nav>
-<section id="nav">
-  <div
-    class="flex justify-between items-center px-8 py-2 shadow-lg fixed top-0 w-full z-1000 bg-white"
-  >
-    <div class="flex items-center gap-3">
-      <img src="../asset/image/cafelogo.png" alt="" class="w-20" />
-      <h1 class="text-dark-brown font-bold text-2xl">Cafe Shop</h1>
-    </div>
-    <nav class="lg:w-2/5 sm:w-3/5 hidden sm:block bg-white" id="menu">
-      <ul class="sm:navbar-b navbar-s">
-        <li>
-          <a href="#home" class="nav-text">Home</a>
-        </li>
-        <li>
-          <a href="" class="nav-text">Shop</a>
-        </li>
-        <li>
-          <a href="#about" class="nav-text">About</a>
-        </li>
-        <li>
-          <a href="#review" class="nav-text">Review</a>
-        </li>
-        <li>
-          <a href="#contact" class="nav-text">Contact</a>
-        </li>
-      </ul>
-    </nav>
-    <button class="block sm:hidden" aria-expanded="false" id="menubtn">
-      <img src="../asset/icon/menu.svg" alt="" width="36" height="36" />
-    </button>
-  </div>
-</section>
